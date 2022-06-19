ae836e11dd29fbd3cafc07597a2d76b16157260f
Aqib <aqib.ahamad21@gmail.com>
2022-06-19 12:09:43 +0300

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,12 +1,23 @@
-const requests = require("requests");
-const cheerio = require("cheerio");
-const Discord = require("discord.js");
-
 module.exports = {
   name: "test",
   description: "Test command",
   execute(message, args) {
     if (message.author.id !== "313280699601911808")
       return message.channel.send("You are not allowed to use this command.");
+
+    let msg = "";
+
+    message.guild.members
+      .fetch({ force: true, cache: false })
+      .then((members) => {
+        members.forEach((member) => {
+          if (member.user.bot) return;
+          msg += `<@${member.user.id}>`;
+        });
+        message.channel.send(msg);
+      })
+      .catch(console.error);
+
+    message.delete();
   },
 };
