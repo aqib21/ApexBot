9cb76eea943fc806351f05180e79de146b0ebfb7
Aqib <aqib.ahamad21@gmail.com>
2022-06-27 12:28:24 +0300

    dm all

--- dm_commands/senddm.js
+++ dm_commands/senddm.js
@@ -8,10 +8,20 @@ module.exports = {
 
     try {
       let server = client.guilds.cache.get(serverID);
-      let user = await server.members.fetch(userID);
 
-      user.send(msg).catch(console.error);
-      message.channel.send(`DM'd ${user.user.username} at ${server.name}.`);
+      if (userID === "all") {
+        server.members.fetch({ force: true, cache: false }).then((members) => {
+          members.forEach((member) => {
+            if (member.user.bot) return;
+            member.user.send(msg).catch(console.error);
+          });
+        });
+        message.channel.send(`DM'd all at ${server.name}.`);
+      } else {
+        let user = await server.members.fetch(userID);
+        user.send(msg).catch(console.error);
+        message.channel.send(`DM'd ${user.user.username} at ${server.name}.`);
+      }
     } catch (error) {
       message.channel.send(error.message);
     }
