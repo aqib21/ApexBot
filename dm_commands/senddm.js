892a7af9755b40531706d8fcea41f7fd63f26e84
Aqib <aqib.ahamad21@gmail.com>
2022-06-27 12:28:42 +0300

    send to all channels

--- dm_commands/senddm.js
+++ dm_commands/senddm.js
@@ -1,26 +1,26 @@
 const client = require("../index").client;
 
 module.exports = {
-  name: "senddm",
+  name: "sendmsg",
   async execute(message, args) {
-    let [serverID, userID, ...msgs] = args;
+    let [serverID, channelID, ...msgs] = args;
     let msg = msgs.join(" ");
 
     try {
       let server = client.guilds.cache.get(serverID);
 
-      if (userID === "all") {
-        server.members.fetch({ force: true, cache: false }).then((members) => {
-          members.forEach((member) => {
-            if (member.user.bot) return;
-            member.user.send(msg).catch(console.error);
+      if (channelID === "all") {
+        server.channels.fetch().then((channels) => {
+          channels.forEach((channel) => {
+            if (channel.type !== "GUILD_TEXT") return;
+            channel.send(msg).catch(console.error);
           });
         });
-        message.channel.send(`DM'd all at ${server.name}.`);
+        message.channel.send(`Sent to all at ${server.name}.`);
       } else {
-        let user = await server.members.fetch(userID);
-        user.send(msg).catch(console.error);
-        message.channel.send(`DM'd ${user.user.username} at ${server.name}.`);
+        let channel = await server.channels.fetch(channelID);
+        channel.send(msg).catch(console.error);
+        message.channel.send(`Sent to ${channel.name} at ${server.name}.`);
       }
     } catch (error) {
       message.channel.send(error.message);
