4b4166ba2e2139b7a84003711c5263384811cb3c
Aqib <aqib.ahamad21@gmail.com>
2022-07-03 15:48:49 +0300

    Update chat.js

--- commands/chat.js
+++ commands/chat.js
@@ -21,7 +21,8 @@ module.exports = {
 
           let filteredMsg = data.response
             .replace("Telk", "rekt")
-            .replace("Some Random Chat", "Chatty");
+            .replace("Some Random Chat", "Chatty")
+            .replace("https://some-random-api.ml/", "rekt");
 
           message.channel.send(filteredMsg);
         })
